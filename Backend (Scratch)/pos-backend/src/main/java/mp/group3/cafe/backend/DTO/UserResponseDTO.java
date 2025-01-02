58b8bc0fa61e0b4e0c25ec75e0a9913a361bfe40
Sheianne Seblante <158006582+seseblante@users.noreply.github.com>
2025-01-02 23:58:21 +0800

    Hides password when posting new user

---
+++ Backend (Scratch)/pos-backend/src/main/java/mp/group3/cafe/backend/DTO/UserResponseDTO.java
@@ -0,0 +1,16 @@
+package mp.group3.cafe.backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+    private Integer userId;
+    private String username;
+    private String role;
+}
--- Backend (Scratch)/pos-backend/src/main/java/mp/group3/cafe/backend/mapper/UserMapper.java
+++ Backend (Scratch)/pos-backend/src/main/java/mp/group3/cafe/backend/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package mp.group3.cafe.backend.mapper;
 
 import mp.group3.cafe.backend.DTO.UserDTO;
+import mp.group3.cafe.backend.DTO.UserResponseDTO;
 import mp.group3.cafe.backend.entities.User;
 
 public class UserMapper {
@@ -20,5 +21,13 @@ public class UserMapper {
         user.setPassword(userDTO.getPassword());
         user.setRole(userDTO.getRole());
         return user;
+    }
+
+    public static UserResponseDTO toUserResponseDTO(UserDTO userDTO) {
+        UserResponseDTO responseDTO = new UserResponseDTO();
+        responseDTO.setUserId(userDTO.getUserId());
+        responseDTO.setUsername(userDTO.getUsername());
+        responseDTO.setRole(userDTO.getRole());
+        return responseDTO;
     }
 }
