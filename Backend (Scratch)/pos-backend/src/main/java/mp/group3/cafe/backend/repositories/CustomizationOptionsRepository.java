9dc42dcf2afa67240b075c40153fe0d43128db6d
Arianne Acosta <adacosta3@up.edu.ph>
2025-01-04 03:34:22 +0800

    Commented smth out

--- Backend (Scratch)/pos-backend/src/main/java/mp/group3/cafe/backend/repositories/CustomizationOptionsRepository.java
+++ Backend (Scratch)/pos-backend/src/main/java/mp/group3/cafe/backend/repositories/CustomizationOptionsRepository.java
@@ -2,7 +2,6 @@ package mp.group3.cafe.backend.repositories;
 
 import mp.group3.cafe.backend.entities.CustomizationOptions;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +9,7 @@ import java.util.List;
 @Repository
 public interface CustomizationOptionsRepository extends JpaRepository<CustomizationOptions, Integer> {
     List<CustomizationOptions> findByCustomization_CustomizationId(Integer customizationId);
-    List<CustomizationOptions> findByItemCode(@Param("itemCode") String itemCode);
+//    List<CustomizationOptions> findByItemCode(@Param("itemCode") String itemCode);
 
 }
 
